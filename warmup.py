e5ae3cbd261b384dbeee8304b1bd728c5de32a71
Min Woo <minwoo@iMac5.local>
2018-12-13 13:40:47 -0500

    made the square bounce off the window wall

--- warmup.py
+++ warmup.py
@@ -4,7 +4,11 @@ import block
 
 
 pygame.init()
-main_window = pygame.display.set_mode((500, 500), 32, 0)
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+X_SPEED = 50
+Y_SPEED = 50
+main_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 32, 0)
 pygame.display.set_caption("Animation")
 
 BLUE = (0, 0, 255)
@@ -13,8 +17,8 @@ WIDTH = 25
 HEIGHT = 25
 
 my_block = block.Block(main_window, WIDTH, HEIGHT, BLUE)
-my_block.rect.x = 10
-my_block.rect.x = 10
+my_block.rect.x = 30
+my_block.rect.y = 10
 main_window.blit(my_block.image, my_block.rect)
 pygame.display.update()
 
@@ -26,7 +30,11 @@ while True:
             sys.exit()
 
     main_window.fill(WHITE)
-    my_block.rect.left += 3
-    my_block.rect.top += 3
+    my_block.rect.left += X_SPEED
+    my_block.rect.top += Y_SPEED
+    if my_block.rect.left <= 0 or my_block.rect.right >= WINDOW_WIDTH:
+        X_SPEED = - X_SPEED
+    if my_block.rect.top <= 0 or my_block.rect.bottom >= WINDOW_HEIGHT:
+        Y_SPEED = - Y_SPEED
     main_window.blit(my_block.image, my_block.rect)
     pygame.display.update()
