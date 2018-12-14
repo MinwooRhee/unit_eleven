77aa6a8530e68bdac5d643db773fff9dbb75283f
Min Woo <minwoo@iMac5.local>
2018-12-14 14:42:24 -0500

    three blocks, random speed

--- warmup.py
+++ warmup.py
@@ -11,17 +11,23 @@ Y_SPEED = 50
 main_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 32, 0)
 pygame.display.set_caption("Animation")
 
+RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 WIDTH = 25
 HEIGHT = 25
 
 my_block = block.Block(main_window, WIDTH, HEIGHT, BLUE)
-my_block.rect.x = 30
-my_block.rect.y = 10
-main_window.blit(my_block.image, my_block.rect)
-pygame.display.update()
+new_block = block.Block(main_window, WIDTH, HEIGHT, GREEN)
+red_block = block.Block(main_window, WIDTH, HEIGHT, RED)
 
+my_block.rect.x = 10
+my_block.rect.y = 10
+new_block.rect.x = WINDOW_WIDTH - 35
+new_block.rect.y = WINDOW_HEIGHT - 35
+red_block.rect.x = 20
+red_block.rect.y = 20
 
 while True:
     for event in pygame.event.get():
@@ -30,11 +36,10 @@ while True:
             sys.exit()
 
     main_window.fill(WHITE)
-    my_block.rect.left += X_SPEED
-    my_block.rect.top += Y_SPEED
-    if my_block.rect.left <= 0 or my_block.rect.right >= WINDOW_WIDTH:
-        X_SPEED = - X_SPEED
-    if my_block.rect.top <= 0 or my_block.rect.bottom >= WINDOW_HEIGHT:
-        Y_SPEED = - Y_SPEED
+    my_block.move()
+    new_block.move()
+    red_block.move()
     main_window.blit(my_block.image, my_block.rect)
+    main_window.blit(new_block.image, new_block.rect)
+    main_window.blit(red_block.image, red_block.rect)
     pygame.display.update()
