03211ccd40e43853bd87e490c315c19937dbabd0
Min Woo <minwoo@iMac5.local>
2019-01-16 11:56:42 -0500

    added some comments and docstrings

--- ball.py
+++ ball.py
@@ -4,6 +4,13 @@ import pygame
 class Ball(pygame.sprite.Sprite):
 
     def __init__(self, color, windowWidth, windowHeight, radius):
+        """
+        initialize the class as sprite, fill the sprite with background color and draw the ball
+        :param color: color of the ball
+        :param windowWidth: width of the application
+        :param windowHeight: height of the application
+        :param radius: radius of the ball
+        """
         super().__init__()
 
         # finish setting the class variables to the parameters
@@ -13,6 +20,8 @@ class Ball(pygame.sprite.Sprite):
         self.radius = radius
         self.x_speed = 0
         self.y_speed = 5
+        # absolute speed of the ball after hitting the paddle
+        self.abs_speed = 15
 
         # Create a surface, get the rect coordinates, fill the surface with a white color (or whatever color the
         # background of your breakout game will be.
@@ -22,9 +31,13 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
         # Add a circle to represent the ball to the surface just created.
-        pygame.draw.circle(self.image, (255, 0, 0), (radius, radius), radius, 0)
+        pygame.draw.circle(self.image, self.color, (radius, radius), radius, 0)
 
     def move(self):
+        """
+        move the ball, speed changing when the ball hits the sides and the top
+        :return: None
+        """
 
         self.rect.x += self.x_speed
         self.rect.y += self.y_speed
@@ -35,10 +48,18 @@ class Ball(pygame.sprite.Sprite):
             self.y_speed = - self.y_speed
 
     def collide(self, bricks_group, paddle_group):
+        """
+        ball collides with the bricks and the paddle
+        :param bricks_group: sprite group of bricks
+        :param paddle_group: sprite group of paddle
+        :return: None
+        """
+
         if pygame.sprite.spritecollide(self, bricks_group, True):
             self.y_speed = - self.y_speed
         if pygame.sprite.spritecollide(self, paddle_group, False):
+            # the x speed of the ball changes relative to the distance between the mouse and the ball
             mouse = pygame.mouse.get_pos()
-            position = self.rect.x + self.radius - mouse[0]
+            position = self.rect.x + self.radius - mouse[0]  # position is a tupple
             self.x_speed = position/5
-            self.y_speed = - abs(15 - abs(position/5))
+            self.y_speed = - abs(self.abs_speed - abs(position/5))
--- breakout.py
+++ breakout.py
@@ -1,7 +1,17 @@
+# Minwoo Rhee
+# 2019 01 16
+# breakout.py
+# move the paddle with mouse to bounce the ball and break bricks
+
 import pygame, sys, brick, paddle, ball
 from pygame.locals import *
 
+
 def main():
+    """
+    Variables are defined, main surface is set, place the bricks, paddle, ball and blit
+    :return: None
+    """
     # Constants that will be used in the program
     APPLICATION_WIDTH = 400
     APPLICATION_HEIGHT = 600
@@ -35,27 +45,33 @@ def main():
     pygame.display.set_caption("breakout")
     main_surface.fill(WHITE)
 
+    # bricks and paddle has to be in group to collide the ball
     bricks_group = pygame.sprite.Group()
     paddle_group = pygame.sprite.Group()
     color_list = [RED, ORANGE, YELLOW, GREEN, CYAN]
 
+    # loop for changing rows
     for a in range(10):
 
-        color = color_list[a//2]
+        # color changes after every two rows
+        brick_color = color_list[a//2]
 
+        # loop for placing 10 bricks in a row
         for b in range(10):
-            my_brick = brick.Brick(BRICK_WIDTH, BRICK_HEIGHT, color)
+            my_brick = brick.Brick(BRICK_WIDTH, BRICK_HEIGHT, brick_color)
             my_brick.rect.x = (BRICK_WIDTH + 4) * b
             my_brick.rect.y = BRICK_Y_OFFSET
             bricks_group.add(my_brick)
 
         BRICK_Y_OFFSET = BRICK_Y_OFFSET + (BRICK_HEIGHT + 4)
 
+    # placing the paddle
     my_paddle = paddle.Paddle(main_surface, BLACK, PADDLE_WIDTH, PADDLE_HEIGHT)
     my_paddle.rect.y = APPLICATION_HEIGHT - PADDLE_Y_OFFSET
     paddle_group.add(my_paddle)
 
-    my_ball = ball.Ball(BLACK, APPLICATION_WIDTH, APPLICATION_HEIGHT, RADIUS_OF_BALL)
+    # placing the ball
+    my_ball = ball.Ball(RED, APPLICATION_WIDTH, APPLICATION_HEIGHT, RADIUS_OF_BALL)
     my_ball.rect.x = APPLICATION_WIDTH/2
     my_ball.rect.y = APPLICATION_HEIGHT/2
 
@@ -65,28 +81,29 @@ def main():
                     pygame.quit()
                     sys.exit()
 
+                # When all bricks are broke, end the program
                 if len(bricks_group) == 0:
                     pygame.quit()
                     sys.exit()
 
-
+            # When the ball hits the bottom, -1 life
             if my_ball.rect.bottom >= APPLICATION_HEIGHT:
                 my_ball.rect.x = APPLICATION_WIDTH / 2
                 my_ball.rect.y = APPLICATION_HEIGHT / 2
                 if NUM_TURNS == 0:
-                    break
+                    break  # end the program when all loves are lost
                 else:
                     NUM_TURNS -= 1
 
+            # fill the surface white so the sprites do not leave traces
             main_surface.fill(WHITE)
 
+            # move and blit paddle
             position = pygame.mouse.get_pos()
-
-            # blit paddle
             my_paddle.rect.x = position[0] - PADDLE_WIDTH/2
             main_surface.blit(my_paddle.image, my_paddle.rect)
 
-            # blit ball
+            # move and blit ball
             my_ball.move()
             main_surface.blit(my_ball.image, my_ball.rect)
             my_ball.collide(bricks_group, paddle_group)
